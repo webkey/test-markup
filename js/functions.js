5531ea5b180887a6f1ca9942e2aec7a3833979f3
Serhii <serhiy.ilchenko@gmail.com>
2015-10-14 22:08:22 +0300

    20151014(1)

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+/* placeholder */
+function placeholderInit(){
+	$('[placeholder]').placeholder();
+}
+/* placeholder end */
+
+/** ready/load/resize document **/
+
+$(document).ready(function(){
+	placeholderInit();
+});
