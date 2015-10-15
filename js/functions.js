b6604f46bd926350ce6d5f75870423853ac34a48
Serhii <serhiy.ilchenko@gmail.com>
2015-10-15 03:33:58 +0300

    20151014(2)

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,42 @@ function placeholderInit(){
 }
 /* placeholder end */
 
+/* Slick Slider Init */
+function slickSliderInit(){
+	$('.slider').slick({
+		dots: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 7500
+	});
+}
+/* Slick Slider Init end */
+
+/*Drop Navigation*/
+function dropNavigation() {
+	$('.js-fn-btn').on('click', function () {
+		var btn = $(this);
+		btn
+			.closest('.js-fn-wrapper')
+			.toggleClass('expanded-nav')
+			.find('.js-fn-nav')
+			.stop()
+			.slideToggle();
+	})
+}
+function clearDrop(){
+	$('.js-fn-nav').attr('style','');
+	$('.js-fn-wrapper').removeClass('expanded-nav');
+}
+/*Drop Navigation end*/
+
 /** ready/load/resize document **/
 
 $(document).ready(function(){
 	placeholderInit();
+	slickSliderInit();
+	dropNavigation();
+});
+$(window).resize(function(){
+	clearDrop();
 });
